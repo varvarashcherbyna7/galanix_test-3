d2f87b060311a6b565481c183c3c99efb3b936e0
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-03-04 14:59:08 +0200

    app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,19 @@ const App = () => {
     useEffect(() => {
         try {
             async function fetchData() {
-                let res = await fetch(`http://universities.hipolabs.com/search?name/?format=json`);
+                let res = await fetch(`http://universities.hipolabs.com/search?name/?format=json`, {
+                    // method: 'GET', // *GET, POST, PUT, DELETE, etc.
+                    // mode: 'same-origin', // no-cors, *cors, same-origin
+                    //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                    //credentials: 'same-origin', // include, *same-origin, omit
+                    // headers: {
+                    //     'Content-Type': 'application/json'
+                    //     // 'Content-Type': 'application/x-www-form-urlencoded',
+                    // },
+                    //redirect: 'follow', // manual, *follow, error
+                    referrerPolicy: 'no-referrer', // no-referrer, *client
+                    //body: "Content-Type" // body data type must match "Content-Type" header
+                });
                 let data = await res.json();
                 setData(data);
             }
